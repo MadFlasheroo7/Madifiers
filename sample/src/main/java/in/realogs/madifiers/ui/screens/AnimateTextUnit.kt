c13945fc42431519c81b25b6e428833f1bb33165
MadFlasheroo7 <jayeshseth45@gmail.com>
2023-07-28 00:00:44 +0530

    add sample for animateTextUnit and windowinfo

--- sample/src/main/java/in/realogs/madifiers/ui/screens/AnimateTextUnit.kt
+++ sample/src/main/java/in/realogs/madifiers/ui/screens/AnimateTextUnit.kt
@@ -1,14 +1,100 @@
 package `in`.realogs.madifiers.ui.screens
 
-import androidx.compose.foundation.border
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import `in`.realogs.animatetextunitasstate.animateTextUnitAsState
+import `in`.realogs.madifiers.ui.theme.MadifiersTheme
 
+@Composable
+fun AnimateTextUnitScreen() {
+    var animateTween by remember { mutableStateOf(true) }
+    var animateSpring by remember { mutableStateOf(true) }
+
+    val textSizeTween by animateTextUnitAsState(
+        targetValue =  if (animateTween) 30.sp else 16.sp,
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = LinearEasing
+        )
+    )
+    val textSizeSpring by animateTextUnitAsState(
+        targetValue =  if (animateSpring) 30.sp else 16.sp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Column {
+        TextCard(
+            text = "Animate Text - tween",
+            onClick = { animateTween = !animateTween },
+            fontSize = textSizeTween
+        )
+        TextCard(
+            text = "Animate Text - spring",
+            onClick = { animateSpring = !animateSpring },
+            fontSize = textSizeSpring
+        )
+    }
+}
+
+@Composable
+fun TextCard(
+    text: String,
+    fontSize: TextUnit = 16.sp,
+    onClick: () -> Unit
+) {
+    Card(modifier = Modifier.padding(8.dp)) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = text,
+                fontSize = fontSize,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(20.dp)
+            )
+            Button(onClick = onClick, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Change Size")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTextCard() {
+    TextCard(text = "Text Size", onClick = {})
+}
+
+@Preview
+@Composable
+fun PreviewAnimateTextUnitScreen() {
+    MadifiersTheme {
+        AnimateTextUnitScreen()
+    }
+}
--- sample/src/main/java/in/realogs/madifiers/ui/screens/WindowInfo.kt
+++ sample/src/main/java/in/realogs/madifiers/ui/screens/WindowInfo.kt
@@ -3,6 +3,7 @@ package `in`.realogs.madifiers.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -55,22 +56,24 @@ fun InfoTemplate(
     info: String,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.fillMaxWidth()) {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(4.dp)
-        )
-        Text(
-            text = info,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
+    Card(modifier = Modifier.padding(8.dp)) {
+        Column(modifier = modifier.fillMaxWidth().padding(8.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+            )
+            Text(
+                text = info,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
     }
 }
 
