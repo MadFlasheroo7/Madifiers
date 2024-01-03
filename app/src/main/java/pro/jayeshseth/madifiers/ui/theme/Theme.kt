57a20a0f74e237c496ec8f1f68974dfb36642dbb
jayeshSeth <jayesh.seth@esmagico.in>
2024-01-04 01:56:42 +0530

    changes: clean code

--- app/src/main/java/pro/jayeshseth/madifiers/ui/theme/Theme.kt
+++ app/src/main/java/pro/jayeshseth/madifiers/ui/theme/Theme.kt
@@ -55,7 +55,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MadifiersTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  // Dynamic color is available on Android 12+
   dynamicColor: Boolean = true,
   content: @Composable () -> Unit,
 ) {
