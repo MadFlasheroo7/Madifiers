9b82db835318b9d4b7d2f9b0fc17f8519b1b33ea
MadFlasheroo7 <jayeshseth45@gmail.com>
2024-01-27 02:29:04 +0530

    changes: add color picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.compose.material3.window)
     implementation(libs.compose.ui)
     implementation(libs.shimmer)
+    implementation(libs.color.picker)
     implementation(libs.navigation.compose)
     implementation(libs.navigation.compose)
     implementation(libs.compose.ui.googleFonts)
--- app/src/main/java/pro/jayeshseth/madifiers/ui/screens/GlowButtonScreen.kt
+++ app/src/main/java/pro/jayeshseth/madifiers/ui/screens/GlowButtonScreen.kt
@@ -17,6 +17,8 @@ package pro.jayeshseth.madifiers.ui.screens
 
 import android.widget.Toast
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,7 +26,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -40,8 +44,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.github.skydoves.colorpicker.compose.BrightnessSlider
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import pro.jayeshseth.buttons.GlowingButton
 import pro.jayeshseth.buttons.GlowingButtonDefaults
 import pro.jayeshseth.madifiers.ui.composables.SliderTemplate
@@ -51,17 +60,58 @@ fun GlowButtonScreen() {
     var spreadRadius by remember { mutableFloatStateOf(GlowingButtonDefaults.spreadRadius.value) }
     var shape by remember { mutableFloatStateOf(50.dp.value) }
     var glowIntensity by remember { mutableFloatStateOf(GlowingButtonDefaults.glowIntensity) }
+    var buttonGlowIntensity by remember { mutableFloatStateOf(0f) }
     var glowRadius by remember { mutableFloatStateOf(GlowingButtonDefaults.glowRadius.value) }
     val enabled = remember { mutableStateOf(true) }
     var glowBorderRadius by remember { mutableFloatStateOf(GlowingButtonDefaults.glowBorderRadius.value) }
     var offsetX by remember { mutableFloatStateOf(0f) }
     var offsetY by remember { mutableFloatStateOf(0f) }
-    val context = LocalContext.current
+    val controller = rememberColorPickerController()
+    val defaultContainerColor = MaterialTheme.colorScheme.primary
+    val defaultContentColor = MaterialTheme.colorScheme.onPrimary
+    val defaultGlowColor = MaterialTheme.colorScheme.primary
 
+    val containerColor = remember { mutableStateOf(defaultContainerColor) }
+    val contentColor = remember { mutableStateOf(defaultContentColor) }
+    val glowColor = remember { mutableStateOf(defaultGlowColor) }
+    val changeContainerColor = remember { mutableStateOf(false) }
+    val changeContentColor = remember { mutableStateOf(false) }
+    val changeGlowColor = remember { mutableStateOf(false) }
+    val showColorPicker = remember { mutableStateOf(false) }
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize(),
     ) {
+        if (showColorPicker.value) {
+            Dialog(
+                onDismissRequest = { showColorPicker.value = !showColorPicker.value },
+            ) {
+                Column {
+                    HsvColorPicker(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(450.dp)
+                            .padding(10.dp),
+                        controller = controller,
+                        onColorChanged = { colorEnvelope ->
+                            if (changeContainerColor.value) containerColor.value =
+                                colorEnvelope.color
+                            if (changeContentColor.value) contentColor.value = colorEnvelope.color
+                            if (changeGlowColor.value) glowColor.value = colorEnvelope.color
+                        },
+                    )
+                    BrightnessSlider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .height(35.dp)
+                            .align(Alignment.CenterHorizontally),
+                        controller = controller,
+                    )
+                }
+            }
+        }
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
@@ -85,6 +135,12 @@ fun GlowButtonScreen() {
                 onDoubleTap = {
                     Toast.makeText(context, "ACTION: Double Tap", Toast.LENGTH_SHORT).show()
                 },
+                colors = GlowingButtonDefaults.glowingButtonColors(
+                    containerColor = containerColor.value,
+                    containerGlowIntensity = buttonGlowIntensity,
+                    contentColor = contentColor.value,
+                    glowColor = glowColor.value,
+                ),
             ) {
                 Text("Madifiers")
             }
@@ -110,6 +166,34 @@ fun GlowButtonScreen() {
                     onCheckedChange = { enabled.value = !enabled.value },
                 )
             }
+            ColorPicker(
+                title = "Container Color",
+                color = containerColor.value,
+                onClick = {
+                    showColorPicker.value = !showColorPicker.value
+                    changeContainerColor.value = true
+                    changeContentColor.value = false
+                    changeGlowColor.value = false
+                },
+            )
+            ColorPicker(
+                title = "Content Color", color = contentColor.value,
+                onClick = {
+                    showColorPicker.value = !showColorPicker.value
+                    changeContentColor.value = true
+                    changeContainerColor.value = false
+                    changeGlowColor.value = false
+                },
+            )
+            ColorPicker(
+                title = "Glow Color", color = glowColor.value,
+                onClick = {
+                    showColorPicker.value = !showColorPicker.value
+                    changeGlowColor.value = true
+                    changeContainerColor.value = false
+                    changeContentColor.value = false
+                },
+            )
             SliderTemplate(
                 title = "Spread Radius: ${spreadRadius.dp}",
                 value = spreadRadius,
@@ -128,6 +212,13 @@ fun GlowButtonScreen() {
                 title = "Glow Intensity: ${glowIntensity}f",
                 value = glowIntensity,
                 onValueChange = { glowIntensity = it },
+                valueRange = -1f..1f,
+                steps = 0,
+            )
+            SliderTemplate(
+                title = "Button Glow Intensity: ${buttonGlowIntensity}f",
+                value = buttonGlowIntensity,
+                onValueChange = { buttonGlowIntensity = it },
                 valueRange = -1f..1f,
                 steps = 0,
             )
@@ -160,5 +251,30 @@ fun GlowButtonScreen() {
                 steps = 10,
             )
         }
+    }
+}
+
+@Composable
+private fun ColorPicker(
+    title: String,
+    color: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = "$title :",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Box(
+            modifier = Modifier
+                .background(color)
+                .size(25.dp)
+                .clickable(onClick = onClick),
+        )
     }
 }
